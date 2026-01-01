0b0dcb5403fe58c90d19172691a29cf4388a8569
rw42 <roguewizard42@gmail.com>
2025-12-31 19:49:07 -0500

    add ssh config check

--- mini-lynis/mini_lynis.py
+++ mini-lynis/mini_lynis.py
@@ -48,6 +48,33 @@ def audit_file_permissions():
         print("   [Pass] No file permission issues found.")
 
 
-# temp testing line
-if __name__ == "__main__":
-    audit_file_permissions()
+def audit_ssh_config():
+    print("--- [2] Checking SSH Configuration ---")
+    ssh_config_path = "/etc/ssh/sshd_config"
+
+    try:
+        with open(ssh_config_path, "r") as f:
+            lines = f.readlines()
+
+        root_login_safe = False
+
+        for line in lines:
+            clean_line = line.strip()
+            # filter out empty and commented out lines
+            if not clean_line or clean_line.startswith("#"):
+                continue
+            parts = clean_line.split()
+            if len(parts) >= 2 and parts[0] == "PermitRootLogin":
+                if parts[1] == "no":
+                    root_login_safe = True
+                else:
+                    print(
+                        "[WARNING] 'PermitRootLogin no' not found. It could be (unsafe) or commented out."
+                    )
+
+    except FileExistsError:
+        print(f"[NOTE] {ssh_config_path} not found. Is OpenSSH installed?")
+    except PermissionError:
+        print(
+            f"[FAIL] Permission denied reading {ssh_config_path} Try running with `sudo`."
+        )
