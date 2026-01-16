94b4a398d49ac49a0664460875093227501655fa
rw42 <roguewizard42@gmail.com>
2026-01-16 15:28:12 -0500

    build out project scaffold; start haystack.py

--- haystack/README.md
+++ haystack/README.md
@@ -1,1 +1,9 @@
 haystack log parsing tool
+
+**Goal:** Build a modular command-line tool that parses authentication logs from the systemd journal, normalizes events into a standard format, and detects suspicious patterns (SSH brute force, repeated sudo authentication failures).
+
+- **Limitations:** v1.0 will only address Arch/journald
+
+**This project is currently under construction**
+
+Stay tuned...
--- haystack/haystack.py
+++ haystack/haystack.py
@@ -1,2 +1,33 @@
 # haystack log parsing tool
 
+import argparse
+import sys
+from datetime import timedelta
+from durations import Duration
+
+
+# function to parse the lookback duration and convert to ISO-timestamp
+def parse_duration(string: str):
+    try:
+        return Duration(string).to_timedelta()
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(str(e))
+
+# CLI interface
+def parse_arguments():
+    parser = argparse.ArgumentParser(description = "Haystack: Authentication log parser and threat detector")
+    subparsers = parser.add_subparsers(dest = "command", help = "Available sub-commands")
+
+    # sub-commands
+    scan = subparsers.add_parser("haystack", help = "Begin the scanning process")
+    scan.add_argument("--since", type = parse_duration, default = "1h", help = "Lookback time (eg. 1h, 1d, 30m, 90s). (default: %(default)")
+    scan.add_argument("--threshold", type = int, default = 5, help = "Failed attempts before flagging")
+    scan.add_argument("--output", default = None, help = "Write JSON output to file instead of stdout")
+    scan.add_argument("--verbose", action = 'store_true', help = "Verbose output to stderr")
+
+    args = parser.parse_args()
+    return args
+
+    
+    
+
